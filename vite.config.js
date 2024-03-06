58177abf9c7109061a429c9fe0532ffdfc1ba6c4
xdd <xdd@xdd.com>
2024-03-06 15:59:46 -0500

    fix2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+	base: "https://kraw-codehorde.github.io/ghtest/",
+	
 })
